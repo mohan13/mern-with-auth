38909c3efd4617be4700843ee694e7fb94643d39
mohan13 <mohangrg250@gmail.com>
2025-01-26 00:21:18 +0545

    restore deleted file

---
+++ server/src/Controller/BlogController.js
@@ -0,0 +1,232 @@
+const { default: mongoose } = require("mongoose");
+const Blog = require("../Models/BlogsModel");
+const User = require("../Models/UserModel");
+const cloudinary = require("cloudinary").v2;
+const asyncHandler = require("../util/asyncHandler");
+const { uploadOnCloudinary } = require("../util/cloudinaryConfig");
+
+const getBlogs = asyncHandler(async (req, res) => {
+  const userId = req.user?.id;
+  const data = await Blog.aggregate([
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "user_details",
+      },
+    },
+    // {
+    //   $addFields: {
+    //     auther_details: {
+    //       $arrayElemAt: ["$user_details", 0],
+    //     },
+    //   },
+    // },
+  ]);
+  // const data = await Blog.find({});
+  return res.json({ msg: "All blogs here . . . ", data });
+});
+
+const deleteBlog = asyncHandler(async (req, res) => {
+  let blog = await Blog.findByIdAndDelete(req.params.id);
+  return res.json({ msg: "Blog deleted .... !", blog });
+});
+
+const blogDetails = asyncHandler(async (req, res) => {
+  let details = await Blog.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.params.id),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "user_details",
+      },
+    },
+  ]);
+
+  return res.json({ msg: "All details here", details });
+});
+
+const postedByMe = asyncHandler(async (req, res) => {
+  let myPost = await Blog.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user?._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "user_details",
+      },
+    },
+  ]);
+
+  if (myPost.length === 0) {
+    return res.status(200).json({ msg: "You have no post" });
+  }
+  return res.json({ msg: "All details here", myPost });
+});
+
+// const updateBlog = asyncHandler(async (req, res) => {
+//   const id = req.params.id;
+
+//   const { title, description, category, images } = req.body;
+
+//   const imageLocalPath = req.file?.path;
+//   if (!imageLocalPath && !images) {
+//     return res.status(400).json({ msg: "No file uploaded", imageLocalPath });
+//   }
+
+//   let blogImageUrl = images; // Use the existing image URL if no new image is uploaded
+
+//   console.log("file", req.file?.path);
+
+//   if (imageLocalPath) {
+//     // If a new image is uploaded, handle the deletion of the old image
+
+//     if (images) {
+//       const publicId = images.split("/").pop().split(".")[0];
+//       console.log("publicId", publicId);
+
+//       await cloudinary.uploader.destroy(publicId, (err) => {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log("File deleted successfully");
+//         }
+//       });
+//     }
+//   }
+
+//   const blogImage = await uploadOnCloudinary(imageLocalPath);
+
+//   if (!blogImage || !blogImage.url) {
+//     return res.status(400).json({ msg: "Error while uploading on avatar" });
+//   }
+//   // Use the new image URL
+//   blogImageUrl = blogImage.url;
+
+//   const updateBlog = await Blog.findByIdAndUpdate(
+//     id,
+//     {
+//       title: title,
+//       description: description,
+//       category: category,
+//       images: blogImageUrl,
+//     },
+//     { new: true },
+//   );
+
+//   return res
+//     .status(200)
+//     .json({ msg: "blog updated successfully ", updateBlog });
+// });
+
+const updateBlog = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+
+  const { title, description, category, images } = req.body;
+  const imageLocalPath = req.file?.path;
+
+  console.log("file", req.file, images, req.body);
+
+  if (!imageLocalPath && !images) {
+    return res
+      .status(400)
+      .json({ msg: "No image uploaded and no image URL provided" });
+  }
+
+  let blogImageUrl = images; // Use the existing image URL if no new image is uploaded
+
+  if (imageLocalPath) {
+    // If a new image is uploaded, handle the deletion of the old image
+    if (images) {
+      const publicId = images.split("/").pop().split(".")[0];
+      console.log("publicId", publicId);
+      await cloudinary.uploader.destroy(publicId);
+    }
+
+    try {
+      // Upload the new image to Cloudinary
+      const blogImage = await uploadOnCloudinary(imageLocalPath);
+
+      // Check if the upload was successful and returned a valid object
+      if (!blogImage || !blogImage.url) {
+        return res
+          .status(400)
+          .json({ msg: "Error while uploading image to Cloudinary" });
+      }
+
+      // Use the new image URL
+      blogImageUrl = blogImage.url;
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json({ msg: "Error uploading the new image", error });
+    }
+  }
+
+  try {
+    // Update the blog with the new or existing image
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          title,
+          description,
+          category,
+          images: blogImageUrl,
+        },
+      },
+      { new: true },
+    );
+
+    return res
+      .status(200)
+      .json({ msg: "Blog updated successfully", updatedBlog });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: "Error updating the blog", error });
+  }
+});
+
+const createBlog = asyncHandler(async (req, res) => {
+  const { title, description, category } = req.body;
+  const imageLocalPath = req.file.path;
+  const owner = req.user._id;
+
+  if (!imageLocalPath) {
+    return res.status(400).json({ msg: "No file uploaded", imageLocalPath });
+  }
+
+  const blogImage = await uploadOnCloudinary(imageLocalPath);
+
+  const blogs = await Blog.create({
+    title,
+    description,
+    category,
+    images: blogImage.url,
+    owner,
+  });
+
+  return res.status(200).json({ msg: "Posted successfully ", blogs });
+});
+
+module.exports = {
+  createBlog,
+  getBlogs,
+  deleteBlog,
+  blogDetails,
+  updateBlog,
+  postedByMe,
+};
