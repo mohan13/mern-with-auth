98cbc2160c69ac059fe837ac3dccd64c04aa1bc6
mohan13 <mohangrg250@gmail.com>
2025-01-25 23:59:42 +0545

    updating image successfull

--- server/src/Middlewares/multer.middleware.js
+++ server/src/Middlewares/multer.middleware.js
@@ -2,11 +2,17 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    const allowedFileTypes = ["images/jpg", "image/png", "image/jpeg"];
+
+    if (!allowedFileTypes.includes(file.mimetype)) {
+      cb(new Error("This filetype is not accepted"));
+      return;
+    }
     cb(null, "./public/temp");
   },
 
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
