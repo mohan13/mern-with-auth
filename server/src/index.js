b8c0412a3d1f4fb5c80416840d078a7a28ff7bde
mohan13 <mohangrg250@gmail.com>
2025-01-26 11:14:57 +0545

    removed cloudinary

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,8 @@ app.use(cookieParser());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static("./storage"));
 
 require("dotenv").config();
 
