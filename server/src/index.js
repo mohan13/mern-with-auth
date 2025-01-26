103b46db4d28cfa5d767b5ffb033bbb2f589bf4c
mohan13 <mohangrg250@gmail.com>
2025-01-26 11:04:48 +0545

    kajajflaf

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 require("dotenv").config();
 
