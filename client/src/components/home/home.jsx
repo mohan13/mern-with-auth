5b4a93e0c5be9b57f2df646f67bdc6400f84a961
mohan13 <mohangrg250@gmail.com>
2025-02-07 10:18:41 +0545

    button color changed

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -33,7 +33,7 @@ export const HomeComp = () => {
             </div>
             <Link
               to="#"
-              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-teal-500 hover:bg-teal-600 focus:ring-4 focus:ring-teal-300 dark:focus:ring-teal-900"
             >
               Continue Reading
               <svg
